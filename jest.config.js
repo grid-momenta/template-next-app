8d13eff5dac0e863ae37d3b89afce7c528d3ba8a
DHN Chandan <php.chandan@gmail.com>
2022-05-20 10:34:34 +0600

    test: :wrench: Config tsconfig for test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
+	globals: {
+		"ts-jest": {
+			tsconfig: "./tsconfig.jest.json",
+		},
+	},
 };
