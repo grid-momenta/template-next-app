9e2758ecbe5e77880b5b7180d1c6c6d350701ce2
DHN Chandan <php.chandan@gmail.com>
2022-05-20 12:07:24 +0600

    chore: :wrench: Add jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
 		},
 	},
 	setupFilesAfterEnv: ["./jest.setup.ts"],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 };
