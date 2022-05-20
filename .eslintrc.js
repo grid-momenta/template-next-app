ad2b871bdf296a3f60c9dac3e4b54a271976e933
DHN Chandan <php.chandan@gmail.com>
2022-05-20 11:11:32 +0600

    chore: :wrench: Config lint settings

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+require("@rushstack/eslint-config/patch/modern-module-resolution");
+
+module.exports = {
+	extends: [
+		"@rushstack/eslint-config/profile/web-app",
+		"@rushstack/eslint-config/mixins/react",
+	],
+	parserOptions: { tsconfigRootDir: __dirname },
+	settings: {
+		react: {
+			version: "17.9",
+		},
+	},
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+	rules: {
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				selector: "variable",
+				types: ["boolean"],
+				format: ["PascalCase"],
+				prefix: ["is", "should", "has", "can", "did", "will"],
+			},
+			{
+				selector: "function",
+				format: ["PascalCase", "camelCase"],
+			},
+			{
+				selector: "typeLike",
+				format: ["PascalCase"],
+			},
+		],
+	},
+};
