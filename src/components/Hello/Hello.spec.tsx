cda41fd419bcab6bb2b1090b82d56ac6db74846b
DHN Chandan <php.chandan@gmail.com>
2022-05-20 10:52:20 +0600

    test: :white_check_mark: Add test to check component in document

--- src/components/Hello/Hello.spec.tsx
+++ src/components/Hello/Hello.spec.tsx
@@ -1,7 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import Hello from "./Hello";
 
-it('renders "Hello', () => {
+it('renders "Hello"', () => {
 	render(<Hello />);
+
+	const helloComponent = screen.getAllByText("Hello");
+	expect(helloComponent.length).toBe(1);
+	expect(helloComponent[0]).toBeInTheDocument();
 });
