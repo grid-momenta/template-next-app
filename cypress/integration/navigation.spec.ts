9fddcbc880e1b9e76050300302313be7d72c69ef
DHN Chandan <php.chandan@gmail.com>
2022-05-21 23:34:21 +0600

    test: :white_check_mark: Test in browser and mobile display

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -3,7 +3,7 @@
 context("Navigation", () => {
 	it("should navigate to the about page", () => {
 		// Start from the index page
-		cy.visit("http://localhost:3000/");
+		cy.visit("/");
 
 		// Find a link with an href attribute containing "about" and click it
 		cy.get('a[href*="about"]').click();
