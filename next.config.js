7c52aa4ab55d05a6c0f0733c7db672a02635c1d1
DHN Chandan <php.chandan@gmail.com>
2022-05-31 10:47:22 +0600

    chore: :wrench: Add nextjs support for importing external ts files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	experimental: {
+		// external ts files
+		externalDir: true,
+	},
 };
 
 module.exports = nextConfig;
