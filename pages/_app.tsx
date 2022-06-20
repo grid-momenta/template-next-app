fef58bf59c44d6d2dafe8553e553474898b35329
DHN Chandan <php.chandan@gmail.com>
2022-06-21 02:47:37 +0600

    refactor: :wrench: Connect redux to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import type { AppProps } from "next/app";
 import { ReactElement } from "react";
+import type { AppProps } from "next/app";
 import Head from "next/head";
+
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
+import { Provider } from "react-redux";
+
 import theme from "../theme/lightTheme";
 import createEmotionCache from "../theme/createEmotionCache";
+import { store } from "../store";
 import "../styles/globals.css";
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -18,16 +22,18 @@ interface IAppProps extends AppProps {
 function MyApp(props: IAppProps): ReactElement {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 	return (
-		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</CacheProvider>
+		<Provider store={store}>
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+				</Head>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</CacheProvider>
+		</Provider>
 	);
 }
 
