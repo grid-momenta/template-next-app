095d5465ce7b1134c5ba4e4fd75db4609bc16b55
DHN Chandan <php.chandan@gmail.com>
2022-06-21 16:47:41 +0600

    refactor: :recycle: Make all logs in development mode only

--- store/index.ts
+++ store/index.ts
@@ -1,17 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
-import logger from "redux-logger";
+import { createLogger } from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
+const middleware: Middleware[] = [];
+
+if (process.env.NODE_ENV === "development") {
+	const logger = createLogger({
+		collapsed: true,
+		duration: true,
+	});
+	middleware.push(logger);
+}
+
 export const store = configureStore({
 	reducer: rootReducer,
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
+	devTools: process.env.NODE_ENV === "development", // Show devTools only in development
 });
 
 export const wrapperStore = () => store;
 
 // eslint-disable-next-line @rushstack/typedef-var
 export const wrapper = createWrapper(wrapperStore, {
-	debug: process.env.NODE_ENV === "development",
+	debug: process.env.NODE_ENV === "development", // Console debug output only in development
 });
