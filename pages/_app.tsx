40b2d9337d6433ca97235d18177349b5e39b7c57
DHN Chandan <php.chandan@gmail.com>
2022-06-22 12:48:48 +0600

    fix: :art: Fix server and client side class name conflict

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
+import { StylesProvider, createGenerateClassName } from "@mui/styles";
 
 import theme from "../theme/lightTheme";
 import createEmotionCache from "../theme/createEmotionCache";
@@ -18,18 +19,25 @@ interface IAppProps extends AppProps {
 	emotionCache?: EmotionCache;
 }
 
+// Fix for server and client side class name conflict
+const generateClassName = createGenerateClassName({
+	productionPrefix: "c",
+});
+
 function MyApp(props: IAppProps): ReactElement {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 	return (
 		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider theme={theme}>
-				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-				<CssBaseline />
-				<Component {...pageProps} />
-			</ThemeProvider>
+			<StylesProvider generateClassName={generateClassName}>
+				<Head>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+				</Head>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</StylesProvider>
 		</CacheProvider>
 	);
 }
